a204eca13b4a6a2a6ab2ee48d4b5d344fc859249
Andy <andreas.matt98@web.de>
2021-04-17 01:44:31 +0200

    Querysets and display methods for the filtered index page

--- ooh/views.py
+++ ooh/views.py
@@ -5,7 +5,7 @@ from django.template import Context
 from .models import Location, EventLocation, Event, OohUser, Participate, Question, ChoiceOption, UserSelection, EventCategory, LocationCategory
 from .forms import UserLoginForm, RegLoginSwitch, OohUserCreationForm, UserLocation
 from django.contrib.auth import authenticate, login
-from django.db.models import Q
+from django.db.models import Q, Count, Sum
 
 
 from django.contrib.auth import get_user_model
@@ -16,14 +16,6 @@ from django.utils.decorators import method_decorator
 import datetime
 
 User = get_user_model()
-# Create your views here.
-
-
-
-# def index(request):
-#     # return HttpResponse("Bämski Index.")
-#     context = {"body_id": "b_home"} #, "user": User}
-#     return render(request, 'ooh/index.html', context=context)
 
 class index(generic.ListView):
     model = Event
@@ -32,23 +24,78 @@ class index(generic.ListView):
     def get_queryset(self):
         # //TODO filter from user has to be set here!
         # User filter
-        filt = UserSelection.objects.filter(user=self.request.user).filter(questionRun=self.request.user.currentQuestionRun).filter(valid=True)
-        print("Filter {0}".format(filt.count()))
-        objs = Event.objects.filter(Q(starttime__gte=datetime.date.today()))
-        for f in filt:
-            _cat = f.selection.text
-            print(_cat)
-            # get object for eventCategory
-            # //TODO filter macht nur scheiße, fixen
-            ecat = EventCategory.objects.filter(name__iexact=_cat)
-            lcat = LocationCategory.objects.filter(name__iexact=_cat)
-            print("Filterlängen: {0} | {1}".format(ecat.count(), lcat.count()))
-            objs = objs.filter(
-                Q(eventTemplate__eventCategory__in=ecat) | 
-                Q(eventTemplate__eventLocation__categories__in=lcat) 
-            )
-            
-        return objs.order_by('starttime')
+        if not self.request.user.is_authenticated:
+            return None
+        # Evaluation order needed!
+
+        lcat = LocationCategory.objects.filter(
+            choiceoption__userselection__user=self.request.user,
+            choiceoption__userselection__questionRun=self.request.user.currentQuestionRun,
+            choiceoption__userselection__valid=True,
+            # choiceoption__question__firstQuestion=True
+        ).order_by('choiceoption__question__priority_in_filtering').distinct()
+        ecat = EventCategory.objects.filter(
+            choiceoption__userselection__user=self.request.user,
+            choiceoption__userselection__questionRun=self.request.user.currentQuestionRun,
+            choiceoption__userselection__valid=True,
+            # choiceoption__question__firstQuestion=True
+        ).order_by('choiceoption__question__priority_in_filtering').distinct()
+        print(lcat.exists(), "|", ecat.exists())
+        for l in lcat:
+            print("Location:", l)
+        for e in ecat:
+            print("Event:", e)
+
+        
+        
+        events1 = Event.objects.filter(
+            Q(starttime__gte=datetime.date.today()) &
+            Q(eventTemplate__eventLocation__categories__in=lcat) |
+            Q(starttime__gte=datetime.date.today()) &
+            Q(eventTemplate__eventCategory__in=ecat)
+        ).annotate(
+            num_fitting_location_categories=Count('eventTemplate__eventLocation__categories')
+        ).annotate(
+            num_fitting_event_categories=Count('eventTemplate__eventCategory')
+        ).order_by('-num_fitting_location_categories', '-num_fitting_event_categories', 'starttime').distinct()
+        # ).order_by('num_fitting_location_categories').distinct()
+        
+        
+        print("Found {0} matching lcategories and {1} matching ecategories".format(
+            events1[0].num_fitting_location_categories, events1[0].num_fitting_event_categories))
+        
+        # matches = []
+        for ev in events1:
+            # First print how many matching categories were found
+            print("Event", ev.num_fitting_location_categories, " und ", ev.num_fitting_event_categories)
+            # matches.append(ev.num_fitting_location_categories+ev.num_fitting_event_categories)
+        # print(matches)
+        if events1.exists():
+           pass
+           
+        else:
+            print("There is nothing.")
+            # events2 = events1.filter
+
+        lcat_gen = LocationCategory.objects.filter(
+            choiceoption__userselection__user=self.request.user,
+            choiceoption__userselection__questionRun=self.request.user.currentQuestionRun,
+            choiceoption__userselection__valid=True,
+        )
+        ecat_gen = EventCategory.objects.filter(
+            choiceoption__userselection__user=self.request.user,
+            choiceoption__userselection__questionRun=self.request.user.currentQuestionRun,
+            choiceoption__userselection__valid=True,
+        )
+
+        events = Event.objects.filter(
+            Q(starttime__gte=datetime.date.today()) &
+            Q(eventTemplate__eventLocation__categories__in=lcat_gen) |
+            Q(starttime__gte=datetime.date.today()) &
+            Q(eventTemplate__eventCategory__in=ecat_gen)
+        ).order_by('starttime').distinct()
+        # print("[{0}] {1}".format(events.exists(), events.query))
+        return events
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
         context = super().get_context_data(**kwargs)
@@ -56,62 +103,88 @@ class index(generic.ListView):
         context["body_id"] = "b_home"
         return context
 
+# @userloginrequired
+def questionFinish(request):
+    # if request.method != "POST":
+    #     pass #404 oder so
+    # else:
+    # if request.user.is_authenticated:
+    #     request.user.questionRun = request.user.questionRun + 1
+    #     request.user.save()
+    context = {"body_id": "b_content"} 
+    return render(request, 'ooh/questionend.html', context=context)
+
 def question(request, question_id):
     # return HttpResponse("Bämski Index.")
     if question_id is None or question_id < 0:
         question_id = 1
     question = get_object_or_404(Question, pk=question_id) 
-    
+    choice = -1
     _pq = question.question.all().first().prevQuestion
     # print("PrevQUestion:", pq)
     if _pq is not None:
         pq = _pq.id
     else:
-        pq = -1
-    
-    # user, users questionrun
-    # lastquestion (every already saved answer is ignored)
-    # last question answer
+        pq = question_id
+    if request.user.is_authenticated:
+        user = request.user
+        questionrun = user.currentQuestionRun
+        print("Got User and questionrun")
+        cquestion = Question.objects.get(pk=question_id)
+        try:
+            ans = UserSelection.objects.get(user=user, question=cquestion, questionRun=questionrun)
+            choice = ans.selection.id
+            print("Choice is", choice)
+        except UserSelection.DoesNotExist:
+            choice = -1
+            print("Choice notfound ", choice)
+    else:
+        print("No user")
+        pass
     if request.method == 'POST':
-        if request.user.is_authenticated:
-            user = request.user
-            questionrun = user.currentQuestionRun
-
-            # Get form data //TODO maybe build dynamic form und so
-            # answers = dict(request.POST.lists())
-            # print(answers)
-            # just save the answer of prev question
-            print("Got User and questionrun")
-            if pq > 0:
-                sel = request.POST.get('q{0}'.format(pq))
-                print("PQ:{0}, PQS: {1}".format(pq, sel))
-                
-                pquestion = Question.objects.get(pk=pq)
-                pselection = ChoiceOption.objects.get(pk=sel)
-                # ans = UserSelection(user, pq, sel, questionRun=questionrun)
-                # //TODO bestehende Antworten überschreiben oder sowas
-                ans = UserSelection(user=user, question=pquestion, selection=pselection, questionRun=questionrun)
+        # If currentquestion is the last one, save answer if provided
+        if question.lastQuestion:
+            sel = request.POST.get('q{0}'.format(question_id))
+            if sel is not None and len(sel) > 0:
+                print("Got selection for currentquestion")
+                cselection = ChoiceOption.objects.get(pk=sel)
+                try:
+                    ans = UserSelection.objects.get(user=user, question=question, questionRun=questionrun)
+                    ans.selection = cselection
+                    ans.save()
+                    print("Found Userselection and overwrite it")
+                except UserSelection.DoesNotExist:
+                    ans = UserSelection(user=user, question=question, selection=cselection, questionRun=questionrun)
+                    ans.save()
+                    print("Found no Userselection and created it")
                 print(ans)
                 ans.save()
-            else:
-                print("No prev Q ")
-
+        if pq > 0:
+            sel = request.POST.get('q{0}'.format(pq))
+            print("PQ:{0}, PQS: {1}".format(pq, sel))
+            
+            pquestion = Question.objects.get(pk=pq)
+            pselection = ChoiceOption.objects.get(pk=sel)
+            try:
+                ans = UserSelection.objects.get(user=user, question=pquestion, questionRun=questionrun)
+                ans.selection = pselection
+                ans.save()
+                print("Found Userselection and overwrite it")
+            except UserSelection.DoesNotExist:
+                ans = UserSelection(user=user, question=pquestion, selection=pselection, questionRun=questionrun)
+                ans.save()
+                print("Found no Userselection and created it")
+            print(ans)
+            ans.save()
         else:
-            # do not save anything
-            print("No user")
-            pass
+            print("No prev Q ")
     else:
         print("No POST Method")
-            
-    context = {"body_id": "b_content", "question": question, "curquestionkey": "q{0}".format(question_id), "prevquestion":  pq} 
+    # selection = UserSelection.objects.get(user=user, question=pquestion, questionRun=questionrun)
+    context = {"body_id": "b_content", "question": question, "curquestionkey": "q{0}".format(question_id), "prevquestion":  pq, "checkeditem": choice} 
     return render(request, 'ooh/fragen.html', context=context)
 
-def questionFinish(request):
-    pass
-# class Question(generic.DetailView):
-#     template_name="ooh/fragen.html"
-#     model = Question
-#     context_object_name = "question"
+
 
 class UserLogoutView(LogoutView):
     template_name = 'ooh/logout.html'
@@ -153,19 +226,6 @@ class UserLoginView(LoginView, ProcessFormView):
                     us.save()
                     return JsonResponse({'success': 'Registrierung war erfolgreich.'})
 
-                    # Cant Login user when he must confirm his email in future!
-                    # email = form.cleaned_data.get('email')
-                    # pw = form.cleaned_data.get('password1')
-                    # user = authenticate(request,username=email,password=pw)
-                    # if user is not None:
-                    #     # login(request)
-                    #     login(request, user)
-                    #     print("Successful logged in and registered "+ user.email)
-                    #     # return redirect('index')
-                    #     return JsonResponse({'success': 'Registrierung war erfolgreich.'})
-                    # else:
-                    #     print("Hm.... user is None!")
-                    #     return JsonResponse({'error': 'Login erfolglos.'})
                 else:
                     print("ELSE")
                     print(form.errors)
