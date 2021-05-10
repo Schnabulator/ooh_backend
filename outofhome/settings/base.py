d722710acf941411614f884ab9b06d427b6ca13c
Andy <andreas.matt98@web.de>
2021-05-10 17:11:18 +0200

    using env file for secret key

--- outofhome/settings/base.py
+++ outofhome/settings/base.py
@@ -10,11 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 from pathlib import Path
+import environ
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
+env = environ.Env()
+environ.Env.read_env()
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')m5kp$%gxtjlm=t4v6qw5@)3w((a-fm0vpulxk(cxr!&jvs*$3'
+SECRET_KEY = env("SECRET_KEY")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
